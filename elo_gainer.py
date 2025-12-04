f1a434d82e893642474324e1cf84b8ba67d51f48
Geeniee1 <stockfeltedwind@gmail.com>
2025-12-04 14:55:46 +0100

    Refining the class structure of the project. Changing the inheritance structure so that basically everything is methods and attributes. Starting with a base elo gainer object, anything that can gain an elo. See team and player for additional attributes.

--- elo_gainer.py
+++ elo_gainer.py
@@ -1,7 +1,7 @@
-class elo_gainer():
-    def __init__(self, elo, match_week):
-        self.elo = elo
+class elo_gainer:
+    def __init__(self, rating: float, match_week: int):
+        self.rating = rating
         self.match_week = match_week
-    
-    def elo_change(self, previous_elo):
-        return self.elo - previous_elo
+
+    def elo_change(self, previous_elo: float) -> float:
+        return self.rating - previous_elo
