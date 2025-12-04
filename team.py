61e8ca39f0833e3f5243bcb86698bef5fa5c6f7d
Geeniee1 <stockfeltedwind@gmail.com>
2025-12-04 15:02:13 +0100

    The class for a team object. We can now extract any elo at any given matchweek as a method elo_at.

--- team.py
+++ team.py
@@ -1,7 +1,27 @@
 from python_elo import elo_gainer
 
 
-class team(elo_gainer):
-    def __init__(self, name, rating=1500):
+class Team(elo_gainer):
+    def __init__(self, name: str, rating: float = 1500.0):
         super().__init__(rating, match_week=0)
         self.name = name
+        self.history = []  # [{matchweek, date, elo_before, elo_after, delta}]
+
+    def record(self, matchweek: int, date, elo_before: float, elo_after: float):
+        self.history.append({
+            "matchweek": matchweek,
+            "date": date,
+            "elo_before": elo_before,
+            "elo_after": elo_after,
+            "delta": elo_after - elo_before,
+        })
+
+    def elo_at(self, matchweek: int) -> float:
+        if not self.history:
+            return self.rating
+        last = self.rating
+        for h in self.history:
+            if h["matchweek"] > matchweek:
+                break
+            last = h["elo_after"]
+        return last
