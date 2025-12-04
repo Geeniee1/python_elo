22afa99d28bee1a6436c4a257cc1f3faf2d8f908
Geeniee1 <stockfeltedwind@gmail.com>
2025-12-04 15:14:18 +0100

    data_cleaner is now the class MatchDataProvider, leagues and season are attributes. Later one we will have sport and adjust functionality accordingly (TODO is UFC, NBA to begin with). Changed the functions in to methods of MathDataProvider objects. The Read method work for MathHistory, our primary source for anything football related. However, in the future, the sport attribute will change to to mathe NBA and UFC obviously. And perhaps, there are better sources for the other fotball leagues, that is a future problem.

--- data_cleaner.py
+++ data_cleaner.py
@@ -1,54 +1,27 @@
-from tokenize import String
-from python_elo.player import player
-from python_elo.team import team
 import pandas as pd
-import soccerdata as sd # Dunno why pylance is complaining
-from python_elo.elo_calc import elo_calc
+import soccerdata as sd
 
-class data_cleaner(player, team):
-    class league():
-        def __init__(self, name: str, teams: list, mathweek: int):
-            self.name = name
-            self.teams = teams
-            self.matchweek = mathweek
-    def __init__(self, league, season):
+
+class MatchDataProvider:
+    def __init__(self, league: str = "ENG-Premier League", season: int = 24):
         self.league = league
         self.season = season
-        # Additional initialization code can be added here
 
-    # TODO Let's start with season 24/25
-    # load data using soccerdata
+    def read(self) -> pd.DataFrame:
+        mh = sd.MatchHistory(leagues=self.league, seasons=self.season)
+        return mh.read_games()
 
-    def read_match_data(leagues = "ENG-Premier League", seasons = 24) -> pd.DataFrame:
-        # Example function to read match data
-        data = sd.MatchHistory(leagues="ENG-Premier League", seasons=24)
-        return data.read_games()
-
-    def clean_match_data(df: pd.DataFrame) -> pd.DataFrame:
-        # HTG = Home Team Goals, ATG = Away Team Goals, FTR = Full Time Result
-        # mean_home = AvgH
-        # mean_away = AvgA
-        # mean_draw = AvgD
-
-        df = df[['home_team', 'away_team','HTG', 'ATG', 'FTR', 'AvgH', 'AvgD', 'AvgA']]
-        return df
-
-    df = read_match_data() # read math data for 24/25 premier league
-    df = clean_match_data(df) # clean the data to only save the teams playing in the game, and result
-
-    def print_elo(self, mathweek: int) -> str:
-        ''' Function to print Elo ratings for all teams after a given matchweek'''
-        for team in league.teams:
-            print(f"Matchweek {self.matchweek}: Team {team.name} has Elo rating {team.rating}")
-        
-    
-    
-
-
-
-# write tests for the main functionalities
-# write documentation for the main module
-# update readme file
-# implement error handling and logging
-# visualise Elo rating changes over time as graph and as table
-# optimise code for performance if necessary
+    @staticmethod
+    def clean(df: pd.DataFrame) -> pd.DataFrame:
+        return df[[
+            'home_team', 'away_team', 'HTG', 'ATG', 'FTR', 'AvgH', 'AvgD', 'AvgA', 'Date', 'MW'
+        ]].rename(columns={
+            'HTG': 'home_goals',
+            'ATG': 'away_goals',
+            'FTR': 'result',
+            'AvgH': 'avgH',
+            'AvgD': 'avgD',
+            'AvgA': 'avgA',
+            'Date': 'date',
+            'MW': 'matchweek'
+        })
